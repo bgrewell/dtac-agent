192608734b1bd3c0590fb4214c67cf6e97e52663
Ben Grewell <BGrewell@gmail.com>
2020-11-17 10:42:02 -0800

    fix typo

--- network/interfaces_darwin.go
+++ network/interfaces_darwin.go
@@ -7,5 +7,5 @@ import (
 )
 
 func GetInterfaceStats(name string) (stats *InterfaceStats, err error) {
-	return nil, fmt.Errorf("this function has not been implemented on this operating system yet")
+	return nil, fmt.Errorf("this function has not been implemented on macOS yet")
 }
--- network/interfaces_win.go
+++ network/interfaces_win.go
@@ -5,5 +5,5 @@ package network
 import "fmt"
 
 func GetInterfaceStats(name string) (stats *InterfaceStats, err error) {
-	return nil, fmt.Errorf("this function has not been implemented on this operating system yet")
+	return nil, fmt.Errorf("this function has not been implemented on Windows yet")
 }
