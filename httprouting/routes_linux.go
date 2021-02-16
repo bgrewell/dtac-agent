2f5b21074f65397dc5f03fdff4d492ab370b6f5b
Ben Grewell <BGrewell@gmail.com>
2021-02-16 06:56:23 -0800

    removed dnat code from feature branch

--- httprouting/routes_linux.go
+++ httprouting/routes_linux.go
@@ -3,13 +3,8 @@
 package httprouting
 
 import (
-	"github.com/BGrewell/system-api/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func AddOSSpecificHandlers(r *gin.Engine) {
-	r.GET("/network/iptables/dnat", handlers.GetIptablesDnatRulesHandler)
-	r.GET("examples/network/iptables/dnat/", handlers.GetIptablesDnatExamplesHandler)
-	r.POST("/network/iptables/dnat/:id", handlers.CreateIptablesDnatRuleHandler)
-	r.DELETE("/network/iptables/dnat/:id", handlers.DeleteIptablesDnatRuleHandler)
 }
