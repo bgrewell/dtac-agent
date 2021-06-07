873d2e07ddab9d3a95df7d0fb1eb59aa8a1bc6ba
Ben Grewell <benjamin.grewell@intel.com>
2021-06-07 16:09:50 -0700

    add windows wifi watchdog

--- handlers/wifi_watchdog_windows.go
+++ handlers/wifi_watchdog_windows.go
@@ -4,11 +4,13 @@ import (
 	. "github.com/BGrewell/system-api/common"
 	"github.com/BGrewell/system-api/configuration"
 	"github.com/BGrewell/wifi-watchdog"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"time"
 )
 
 var (
-	Watchdog wifi-watchdog.WifiWatchdog
+	Watchdog watchdog.WifiWatchdog
 )
 
 func init() {
@@ -17,9 +19,16 @@ func init() {
 		for {
 			cfg, err := configuration.GetActiveConfig()
 			if err == nil {
-
+				Watchdog = watchdog.WifiWatchdog{}
+				if cfg.Watchdog.Enabled {
+					Watchdog.Start(cfg.Watchdog.Profile, cfg.Watchdog.PollInterval)
+					log.Printf("wifi watchdog started. watching for profile: %s every %d seconds\n", cfg.Watchdog.Profile, cfg.Watchdog.PollInterval)
+				}
+				break
 			}
+			time.Sleep(30 * time.Second)
 		}
+
 	}()
 }
 
