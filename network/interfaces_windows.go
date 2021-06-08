c82ea68b92caceb696091213d3506b69345f7a4c
Ben Grewell <BGrewell@gmail.com>
2021-06-08 11:16:17 -0700

    add windows network stats

--- network/interfaces_windows.go
+++ network/interfaces_windows.go
@@ -2,8 +2,25 @@
 
 package network
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"github.com/BGrewell/go-execute"
+)
+
+var (
+	statsCache = make(map[string]*InterfaceStats)
+)
 
 func GetInterfaceStats(name string) (stats *InterfaceStats, err error) {
+	cmd := fmt.Sprintf("Get-NetAdapter -Name %s | Get-NetAdapterStatistics | Format-List -Property \"*\"", name)
+	output, stderr, err := execute.ExecutePowershell(cmd)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(strings.TrimSpace(output), "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
 	return nil, fmt.Errorf("this function has not been implemented on Windows yet")
 }
