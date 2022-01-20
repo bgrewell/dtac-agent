a9879c86a3503d4e5050707c0255b122dffe02f4
Ben Grewell <BGrewell@gmail.com>
2022-01-20 04:52:38 -0800

    move scripts

--- go.mod
+++ go.mod
@@ -39,7 +39,7 @@ require (
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
 	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
-	google.golang.org/genproto v0.0.0-20210728212813-7823e685a01f // indirect
+	google.golang.org/genproto v0.0.0-20210729151513-df9385d47c1b // indirect
 	google.golang.org/grpc v1.39.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
--- support/scripts/push-upgrades.sh
+++ support/scripts/push-upgrades.sh
@@ -2,14 +2,27 @@
 
 UPGRADE_CMD="(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://server.cloud.wirelesstrial.net/systemapi/upgrade.ps1') -UseBasicParsing |iex"
 
-for SYSTEM in 192.168.10.101 192.168.10.102 192.168.10.103 192.168.10.104 192.168.10.105 192.168.10.106 192.168.10.107 192.168.10.108 192.168.10.109 192.168.10.110 192.168.10.111 192.168.10.112 192.168.10.113 192.168.10.114 192.168.10.115 192.168.10.118 192.168.10.119
+for SYSTEM in 192.168.10.101 192.168.10.102 192.168.10.103 192.168.10.104 192.168.10.105 192.168.10.106 192.168.10.107 192.168.10.108 192.168.10.109 192.168.10.110 192.168.10.111 192.168.10.112 192.168.10.113 192.168.10.114 192.168.10.115
 do
   echo $SYSTEM
   ssh intel@$SYSTEM -C $UPGRADE_CMD
 done
 
-for SERVER in 10.108.1.21 10.108.2.61
+SERVERS=( 10.108.1.21 10.108.2.61 10.108.1.11 )
+
+for server in "${SERVERS[@]}"
 do
-  echo $SERVER
-  ssh intel@$SERVER -C "curl http://10.108.2.61/systemapi/upgrade.sh | bash"
+  echo "deploying to $server"
+  scp bin/system-apid intel@$server:/home/intel/.
+  scp support/service/system-apid.service intel@$server:/home/intel/.
+  scp support/config/config.yaml intel@$server:/home/intel/.
+  ssh intel@$server -C 'sudo systemctl stop system-apid || true'
+  ssh intel@$server -C 'sudo mkdir -p /opt/system-api/bin || true'
+  ssh intel@$server -C 'sudo mkdir -p /etc/system-api || true'
+  ssh intel@$server -C 'sudo mv ~/system-apid /opt/system-api/bin/.'
+  ssh intel@$server -C 'sudo mv ~/system-apid.service /lib/systemd/system/.'
+  ssh intel@$server -C 'sudo mv ~/config.yaml /etc/system-api/config.yaml'
+  ssh intel@$server -C 'sudo systemctl daemon-reload'
+  ssh intel@$server -C 'sudo systemctl start system-apid'
+  echo "finished deploying to $server"
 done
