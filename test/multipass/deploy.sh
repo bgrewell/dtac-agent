11b08535610be6516bdc0491f98fc867c2fdda7d
Benjamin Grewell <benjamin.grewell@intel.com>
2023-10-19 14:56:03 -0700

    feat: add random password gen
    add generation of random password when installed from a deb package

---
+++ test/multipass/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Ensure that it doesn't already exist
+multipass delete dtac-test
+multipass purge
+
+# Create VM
+multipass launch -n dtac-test
+
+# Copy the files
+multipass transfer dist/*.deb dtac-test:/home/ubuntu
